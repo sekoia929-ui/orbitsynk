949e82d9431b05646476484931be9f4620a3aceb
Mohd Nabeel <mohdnabeel@Mohds-MacBook-Air.local>
2026-06-02 02:19:04 +0500

    refactor: use relative imports for all landing page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
-import Navbar from '@/components/Navbar'
-import HeroSection from '@/components/HeroSection'
-import IntegrationTicker from '@/components/IntegrationTicker'
-import ProblemSection from '@/components/ProblemSection'
-import SolutionSection from '@/components/SolutionSection'
-import HowItWorksSection from '@/components/HowItWorksSection'
-import FeaturesSection from '@/components/FeaturesSection'
-import SocialProofSection from '@/components/SocialProofSection'
-import PricingSection from '@/components/PricingSection'
-import FAQSection from '@/components/FAQSection'
-import CTASection from '@/components/CTASection'
-import Footer from '@/components/Footer'
+import Navbar from '../components/Navbar'
+import HeroSection from '../components/HeroSection'
+import IntegrationTicker from '../components/IntegrationTicker'
+import ProblemSection from '../components/ProblemSection'
+import SolutionSection from '../components/SolutionSection'
+import HowItWorksSection from '../components/HowItWorksSection'
+import FeaturesSection from '../components/FeaturesSection'
+import SocialProofSection from '../components/SocialProofSection'
+import PricingSection from '../components/PricingSection'
+import FAQSection from '../components/FAQSection'
+import CTASection from '../components/CTASection'
+import Footer from '../components/Footer'
+
 
 export default function Home() {
   return (
